206f7f5a984530437784b58209b60494bc590649
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2022-12-19 02:47:37 +0800

    Fix scss for buttons in product details

--- final_proj/src/ProductDetail.js
+++ final_proj/src/ProductDetail.js
@@ -83,15 +83,6 @@ const ProductDetail = () => {
                     </NavLink>
                     </div>
                 </div>
-                {/* <div class="float-container">
-                    <div class="float-child">
-                    <div class="green">Float Column 1</div>
-                    </div>
-
-                    <div class="float-child">
-                    <div class="blue">Float Column 2</div>
-                    </div>
-                </div> */}
             </div>
         </div>
 
