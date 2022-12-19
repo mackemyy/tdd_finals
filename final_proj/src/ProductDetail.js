d98203febfdadd80c065ca72af18840cab347e6b
harbii <vonnehrvy98@gmail.com>
2022-12-19 13:03:15 +0800

    changed a button into Buy It Now

--- final_proj/src/ProductDetail.js
+++ final_proj/src/ProductDetail.js
@@ -76,7 +76,7 @@ const ProductDetail = () => {
                     </div>
                     <div className="buttons">
                     <NavLink to='/login' data-testid='products-nav'>
-                        <button id="backToMenu-btn" className="btns" data-testid="backToMenu-btn">Proceed to Checkout</button>
+                        <button id="backToMenu-btn" className="btns" data-testid="backToMenu-btn">Buy It Now</button>
                     </NavLink>
                     <NavLink to='/products' data-testid='products-nav'>
                         <button id="addToCart-btn" className="btns" data-testid="addToCart-btn" onClick={onAddToCart}>Add to Cart</button>
