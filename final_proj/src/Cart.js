d9e7dd68a0883d648efee9a440437d320257776d
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2022-12-19 15:41:54 +0800

    Fix scrollbar in my cart page

--- final_proj/src/Cart.js
+++ final_proj/src/Cart.js
@@ -16,38 +16,81 @@ const Cart = () => {
                     <p id="total">Total</p>
                 </div>
                 <div className="contents">
+                    <div className="allCarts">
                     <div className="select">
-                        <label className="cart-container">Select All
-                        <input type="checkbox" data-testid="checkbox"/> 
-                            <span class="checkmark"></span>  
-                        </label>
-                    </div>
-                    <div className="cart-items">
-                        <div className="prod">
-                            <img id="prodimg" alt =""></img>
-                        </div>
-                        <div className="prodinfo">
-                            <label className="cart-container">Baked Croissant Bread
+                            <label className="cart-container">Select All
                             <input type="checkbox" data-testid="checkbox"/> 
-                            <span class="checkmark"></span> 
-                            <p className="price">75.00 PHP</p>
+                                <span class="checkmark"></span>  
                             </label>
                         </div>
-                    <div className="qtybtn">
-                        <button className="addBtn">+</button>
-                        <p className="item-qty">1</p>
-                        <button className="subtractBtn">-</button>
-                        <p className="total1">PHP 75.00</p>
-                        <button className="removeBtn">Remove</button>
+                        <div className="cart-items1">
+                            <div className="prod">
+                                <img id="prodimg" alt =""></img>
+                            </div>
+                            <div className="prodinfo">
+                                <label className="cart-container">Baked Croissant Bread
+                                <input type="checkbox" data-testid="checkbox"/> 
+                                <span class="checkmark"></span> 
+                                <p className="price">75.00 PHP</p>
+                                </label>
+                            </div>
+                            <div className="qtybtn">
+                                <button className="addBtn">+</button>
+                                <p className="item-qty">1</p>
+                                <button className="subtractBtn">-</button>
+                                <p className="total1">PHP 75.00</p>
+                                <button className="removeBtn">Remove</button>
+                            </div>
+                        </div>
+                        <div className="cart-items2">
+                            <div className="prod">
+                                <img id="prodimg" alt =""></img>
+                            </div>
+                            <div className="prodinfo">
+                                <label className="cart-container">Baked Croissant Bread
+                                <input type="checkbox" data-testid="checkbox"/> 
+                                <span class="checkmark"></span> 
+                                <p className="price">75.00 PHP</p>
+                                </label>
+                            </div>
+                            <div className="qtybtn">
+                                <button className="addBtn">+</button>
+                                <p className="item-qty">1</p>
+                                <button className="subtractBtn">-</button>
+                                <p className="total1">PHP 75.00</p>
+                                <button className="removeBtn">Remove</button>
+                            </div>
+                        </div>
+                        <div className="cart-items3">
+                            <div className="prod">
+                                <img id="prodimg" alt =""></img>
+                            </div>
+                            <div className="prodinfo">
+                                <label className="cart-container">Baked Croissant Bread
+                                <input type="checkbox" data-testid="checkbox"/> 
+                                <span class="checkmark"></span> 
+                                <p className="price">75.00 PHP</p>
+                                </label>
+                            </div>
+                            <div className="qtybtn">
+                                <button className="addBtn">+</button>
+                                <p className="item-qty">1</p>
+                                <button className="subtractBtn">-</button>
+                                <p className="total1">PHP 75.00</p>
+                                <button className="removeBtn">Remove</button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="subtotal">
-                        <p>Subtotal: PHP 201.00</p>
-                    </div>
-                    <div class="button"> 
-                        <button class="backBtn">Back to Menu</button>
-                        <button class="checkoutBtn">Checkout</button>
-                    </div>
-                    </div>
+                    
+                </div>
+                <div className="footer">
+                        <div className="subtotal">
+                            <p>Subtotal: PHP 201.00</p>
+                        </div>
+                        <div class="button"> 
+                            <button class="backBtn">Back to Menu</button>
+                            <button class="checkoutBtn">Checkout</button>
+                        </div>
                 </div>
             </div>
         </>
