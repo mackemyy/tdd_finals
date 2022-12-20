73f43dcf374cfc830bb70387c9c95c0d353d3f12
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2022-12-21 01:35:44 +0800

    Clean code for Home component

--- final_proj/src/Home.js
+++ final_proj/src/Home.js
@@ -11,11 +11,11 @@ const Home = () => {
                 <p className="home_subtitle">Focusing on creating perfect harmony among textures, flavours and ingredients, our products' recipes are inspired from each of our Pastry Chef's personal take on classic simplicity. Every bite’s a joy as we continue to create products that bring an abundance of joy and goodness in order to satisfy the evolving needs of consumers.</p>
             </div>
             <div>
-            <Link to='/products'>
-                <button className="discoverBtn" data-testid="discover-btn">
-                    Discover Our Pastry 
-                </button>
-            </Link>
+                <Link to='/products'>
+                    <button className="discoverBtn" data-testid="discover-btn">
+                        Discover Our Pastry 
+                    </button>
+                </Link>
             </div>
         </div>
         </>
